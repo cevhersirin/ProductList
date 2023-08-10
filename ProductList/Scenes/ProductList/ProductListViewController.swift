e030f6b3dd354aaf7134f98e1fa37545f4680a71
cevhertom <cevher.sirin@tomdigital.com.tr>
2023-08-10 16:00:52 +0300

    add files

---
+++ ProductList/Scenes/ProductList/ProductListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProductListViewController.swift
+//  ProductList
+//
+//  Created by Cevher Şirin on 10.08.2023.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+}
